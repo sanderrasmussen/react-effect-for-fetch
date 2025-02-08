999f305e614fbb13b08dd0571b349f7c61bf0217
Sander Rasmussen <sander.rasmussn@gmail.com>
2025-02-08 12:30:01 +0100

    added some styling

--- src/sections/Users/components/UsersListItem.tsx
+++ src/sections/Users/components/UsersListItem.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 function UsersListItem(props){
+    let backgroundColor = props.user.favouriteColour +"80"; //adding 80 to get semitransparent background color for nicer presentation
 
     return (
-        <li>
-            <img src={props.user.profileImage} alt="" />
-            <h4>{props.user.firstName}</h4>
+        <li className='userCard' style={{backgroundColor: backgroundColor}}> 
+            <img src={props.user.profileImage} alt="profile picture"  className='profilePicture' />
+            <h4 >{props.user.firstName} {props.user.lastName}</h4>
             <p>Email: {props.user.email}</p>
         </li>
     )
